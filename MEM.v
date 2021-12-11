5f82e4a7852332cf8f9fc43d06845d18a5e6af2c
NEU_Dark <58771668+willer-lu@users.noreply.github.com>
2021-12-11 16:24:20 +0800

    Update MEM.v

--- MEM.v
+++ MEM.v
@@ -29,8 +29,9 @@ module MEM(
     end
 
     wire [31:0] mem_pc;
-    wire data_ram_en;
-    wire [3:0] data_ram_wen;
+    wire [3:0] data_ram_en;
+    wire data_sram_en;
+    wire [3:0] data_ram_wen,sl;
     wire sel_rf_res;
     wire rf_we;
     wire [4:0] rf_waddr;
@@ -39,8 +40,9 @@ module MEM(
     wire [31:0] mem_result;
 
     assign {
+        sl,
         mem_pc,         // 75:44
-        data_ram_en,    // 43
+        data_sram_en,    // 43
         data_ram_wen,   // 42:39
         sel_rf_res,     // 38
         rf_we,          // 37
@@ -49,14 +51,27 @@ module MEM(
     } =  ex_to_mem_bus_r;
 
     assign mem_result = data_sram_rdata;
-    assign rf_wdata = sel_rf_res ? mem_result : ex_result;
-
+    assign rf_wdata =     (sl==4'b0001 && sel_rf_res==1'b1) ? mem_result 
+                        : (sl==4'b0011 && sel_rf_res==1'b1 && ex_result[1:0]==2'b00) ?({{24{mem_result[7]}},mem_result[7:0]})
+                        : (sl==4'b0011 && sel_rf_res==1'b1 && ex_result[1:0]==2'b01) ?({{24{mem_result[15]}},mem_result[15:8]})
+                        : (sl==4'b0011 && sel_rf_res==1'b1 && ex_result[1:0]==2'b10) ?({{24{mem_result[23]}},mem_result[23:16]})
+                        : (sl==4'b0011 && sel_rf_res==1'b1 && ex_result[1:0]==2'b11) ?({{24{mem_result[31]}},mem_result[31:24]})
+                        : (sl==4'b0100 && sel_rf_res==1'b1 && ex_result[1:0]==2'b00) ?({24'b0,mem_result[7:0]})
+                        : (sl==4'b0100 && sel_rf_res==1'b1 && ex_result[1:0]==2'b01) ?({24'b0,mem_result[15:8]})
+                        : (sl==4'b0100 && sel_rf_res==1'b1 && ex_result[1:0]==2'b10) ?({24'b0,mem_result[23:16]})
+                        : (sl==4'b0100 && sel_rf_res==1'b1 && ex_result[1:0]==2'b11) ?({24'b0,mem_result[31:24]})
+                        : (sl==4'b0101 && sel_rf_res==1'b1 && ex_result[1:0]==2'b00) ?({{16{mem_result[15]}},mem_result[15:0]})
+                        : (sl==4'b0101 && sel_rf_res==1'b1 && ex_result[1:0]==2'b10) ?({{16{mem_result[31]}},mem_result[31:16]})
+                        : (sl==4'b0110 && sel_rf_res==1'b1 && ex_result[1:0]==2'b00) ?({16'b0,mem_result[15:0]})
+                        : (sl==4'b0110 && sel_rf_res==1'b1 && ex_result[1:0]==2'b10) ?({16'b0,mem_result[31:16]})
+                        : ex_result;
     assign mem_to_wb_bus = {
         mem_pc,     // 69:38
         rf_we,      // 37
         rf_waddr,   // 36:32
         rf_wdata    // 31:0
     };
+
 
 
 
