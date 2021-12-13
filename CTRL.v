a77ae1e76f9e1ae6d0790ee456c2f372ac6878eb
NEU_Dark <58771668+willer-lu@users.noreply.github.com>
2021-12-13 11:04:40 +0800

    Update CTRL.v

--- CTRL.v
+++ CTRL.v
@@ -1,16 +1,16 @@
 `include "lib/defines.vh"
 module CTRL(
     input wire rst,
-    // input wire stallreq_for_ex,
+    input wire stallreq_for_ex,
     input wire stallreq_for_load,
-
-    // output reg flush,
-    // output reg [31:0] new_pc,
     output reg [`StallBus-1:0] stall
 );  
     always @ (*) begin
         if (rst) begin
             stall = `StallBus'b0;
+        end
+        else if(stallreq_for_ex == `Stop) begin
+            stall = `StallBus'b001111;
         end 
         else if(stallreq_for_load == `Stop) begin
             stall = `StallBus'b000111;
