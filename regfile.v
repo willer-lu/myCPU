a9daeb118e9dfcae66a1683f3d55bbb31b35b15f
NEU_Dark <58771668+willer-lu@users.noreply.github.com>
2021-12-16 13:05:46 +0800

    Update regfile.v

--- regfile.v
+++ regfile.v
@@ -2,14 +2,14 @@
 module regfile(
     input wire clk,
     input wire rst,
-    input wire [4:0] raddr1, //第一个读寄存器 端口 要读取的寄存器的地址
-    output wire [31:0] rdata1,//第一个读寄存器 端口 输出的寄存器值 
+    input wire [4:0] raddr1, 
+    output wire [31:0] rdata1,
     input wire [4:0] raddr2,
     output wire [31:0] rdata2,
     
-    input wire we, //写使能信号
-    input wire [4:0] waddr, //要写入的寄存器地址
-    input wire [31:0] wdata, //要写入的数据
+    input wire we, 
+    input wire [4:0] waddr, 
+    input wire [31:0] wdata, 
     input wire hiwe,
     input wire lowe,
     input wire hir,
@@ -18,10 +18,10 @@ module regfile(
     input wire [31:0] lo_i,
     output wire [31:0] hilodata
 );
-    reg [31:0] reg_array [31:0]; //定义32个32位寄存器
+    reg [31:0] reg_array [31:0]; 
     reg [31:0] HI;
     reg [31:0] LO;
-    // 写端口
+ 
     always @ (posedge clk) begin
         if (we && waddr!=5'b0) begin
             reg_array[waddr] <= wdata;
@@ -45,9 +45,9 @@ module regfile(
 	end
     assign hilodata = (hir)?HI:(lor)?LO:32'b0;
     
-    // 读端口1
+    // 1
     assign rdata1 = (raddr1 == 5'b0) ? 32'b0 : reg_array[raddr1];
 
-    // 读端口2
+    // 2
     assign rdata2 = (raddr2 == 5'b0) ? 32'b0 : reg_array[raddr2];
 endmodule
